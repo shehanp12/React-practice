2f4bf2a3f755fa9e30b69844fee49ab35367e983
shehanp12 <shehanp068@gmail.com>
2020-09-28 22:23:38 +0530

    css addded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import 'rsuite/dist/styles/rsuite-default.css';
+import { Button } from 'rsuite';
+import 'rsuite/lib/styles/index.less';
 
 function App() {
   return (
     <div>
-
+      <Button appearance="primary"> Hello world </Button>
     </div>
   );
 }
